78f64f1e5febdf5eb1bc8bc3c8220f0841a01cd7
shyrapson <meetshyrapson@gmail.com>
2023-11-22 17:59:39 +0100

    final

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -30,13 +30,14 @@ export const NavbarRoutes = () => {
               Exit
             </Button>
           </Link>
-        ) : isTeacher(userId) ? (
+        ) : (
+          //  isTeacher(userId) ?
           <Link href="/teacher/courses">
             <Button size="sm" variant="ghost">
               Teacher Mode
             </Button>
           </Link>
-        ) : null}
+        )}
         <UserButton afterSignOutUrl="/" />
       </div>
     </>
